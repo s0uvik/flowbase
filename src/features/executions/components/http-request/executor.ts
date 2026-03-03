23f79d0abc082a20be1296245b47390697e82de1
Souvik Sarkar <souviksarkar260123@gmail.com>
2026-03-03 20:51:58 +0530

    http node templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,13 +1,21 @@
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, { Options as KyOptions } from "ky";
+import Handlebars from "handlebars";
 
 type HttpRequestData = {
-  variableName?: string;
-  endpoint?: string;
-  method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  variableName: string;
+  endpoint: string;
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
   body?: string;
 };
+
+Handlebars.registerHelper("json", (context) => {
+  const stringified = JSON.stringify(context, null, 2);
+  const safeString = new Handlebars.SafeString(stringified);
+
+  return safeString;
+});
 
 export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
   data,
@@ -27,15 +35,22 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       "HTTP Request node: No variable name configured",
     );
   }
+  if (!data.method) {
+    // TODO: Publish "error" state for http request
+    throw new NonRetriableError("HTTP Request node: No method configured");
+  }
 
   const result = await step.run("http-request", async () => {
     const method = data.method || "GET";
-    const endpoint = data.endpoint!;
+    const endpoint = Handlebars.compile(data.endpoint)(context);
+    console.log(endpoint);
     const body = data.body;
     const options: KyOptions = { method };
 
     if (["POST", "PUT", "PATCH"].includes(method)) {
-      options.body = body;
+      const resolve = Handlebars.compile(body || "{}")(context);
+      JSON.parse(resolve);
+      options.body = resolve;
       options.headers = {
         "Content-Type": "application/json",
       };
@@ -55,19 +70,11 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       },
     };
 
-    if (data.variableName) {
-      return {
-        ...context,
-        [data.variableName]: responsePayload,
-      };
-    }
     return {
       ...context,
-      ...responsePayload,
+      [data.variableName]: responsePayload,
     };
   });
-
-  // TODO: Publish "success" state for http-request
 
   return result;
 };
