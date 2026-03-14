16e187b76a1770df5fe41a2125ede09bc4239b79
Souvik Sarkar <souviksarkar260123@gmail.com>
2026-03-15 00:23:08 +0530

    google & github OAuth

--- src/lib/encryption.ts
+++ src/lib/encryption.ts
@@ -1,6 +1,6 @@
 import Cryptr from "cryptr";
 
-const cryptr = new Cryptr(process.env.ENCRIPTION_KEY!);
+const cryptr = new Cryptr(process.env.ENCRYPTION_KEY!);
 
 export const encrypt = (text: string) => cryptr.encrypt(text);
 export const decrypt = (text: string) => cryptr.decrypt(text);
